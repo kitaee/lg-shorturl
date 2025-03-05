caf83b2efa9914092e4415e211b5484078bbb8e9
kitaee <dlrlxo999@naver.com>
2025-03-05 17:05:50 +0900

    feat: URL 단축 API 구현

--- src/main/java/com/laundrygo/shorturl/domain/UrlMapping.java
+++ src/main/java/com/laundrygo/shorturl/domain/UrlMapping.java
@@ -1,6 +1,7 @@
 package com.laundrygo.shorturl.domain;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,4 +22,17 @@ public class UrlMapping {
     private String shortUrl;
 
     private int requestCount = 0;
+
+    @Builder
+    private UrlMapping(String oriUrl, String shortUrl) {
+        this.oriUrl = oriUrl;
+        this.shortUrl = shortUrl;
+    }
+
+    public static UrlMapping createShortUrl(String oriUrl, String shortUrl) {
+        return UrlMapping.builder()
+                .oriUrl(oriUrl)
+                .shortUrl(shortUrl)
+                .build();
+    }
 }
--- src/main/java/com/laundrygo/shorturl/service/UrlMappingService.java
+++ src/main/java/com/laundrygo/shorturl/service/UrlMappingService.java
@@ -1,7 +1,9 @@
 package com.laundrygo.shorturl.service;
 
+import com.laundrygo.shorturl.domain.UrlMapping;
 import com.laundrygo.shorturl.dto.response.UrlMappingResponse;
 import com.laundrygo.shorturl.repository.UrlMappingRepository;
+import com.laundrygo.shorturl.utils.Base62Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,10 +13,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class UrlMappingService {
 
+    private final Base62Util base62Util;
     private final UrlMappingRepository urlMappingRepository;
 
     @Transactional
     public UrlMappingResponse shortenUrl(String oriUrl) {
-        return null;
+        return urlMappingRepository.findByOriUrl(oriUrl)
+                .map(UrlMappingResponse::of)
+                .orElseGet(() -> UrlMappingResponse.of(createShortUrl(oriUrl)));
+    }
+
+    private UrlMapping createShortUrl(String oriUrl) {
+        return urlMappingRepository.save(UrlMapping.createShortUrl(oriUrl, base62Util.generateShortUrlFromUuid()));
     }
 }
