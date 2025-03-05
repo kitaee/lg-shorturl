1bae0a041132384a0e9f57c4b239d5b6c556a2ae
kitaee <dlrlxo999@naver.com>
2025-03-05 21:19:53 +0900

    add: url_mappings 테이블에 대한 DDL 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,16 @@
 DROP TABLE IF EXISTS Member;
+DROP TABLE IF EXISTS url_mappings;
 
 create table Member
 (
     id integer not null,
     name varchar(255) not null,
     primary key (id)
+);
+
+create table url_mappings (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    ori_url VARCHAR(2083) NOT NULL UNIQUE,
+    short_url VARCHAR(8) NOT NULL UNIQUE,
+    request_count INT DEFAULT 0 NOT NULL
 );
